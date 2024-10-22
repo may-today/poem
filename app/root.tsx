58fff7d7d7b2e91de1c4fb7b98315e4cad3a6916
ddiu8081 <ddiu8081@gmail.com>
2024-10-23 02:02:49 +0800

    chore

--- app/root.tsx
+++ app/root.tsx
@@ -1,30 +1,17 @@
 import {
   Links,
   Meta,
-  Outlet,
+  Form,
   Scripts,
   ScrollRestoration,
+  Outlet,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
 
 import "./tailwind.css";
 
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
-
-export function Layout({ children }: { children: React.ReactNode }) {
+export const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <html lang="en">
+    <html lang="zh-CN">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -32,7 +19,40 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <div id="sidebar">
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form id="search-form" role="search">
+              <input
+                aria-label="Search contacts"
+                id="q"
+                name="q"
+                placeholder="Search"
+                type="search"
+              />
+              <div
+                aria-hidden
+                hidden={true}
+                id="search-spinner"
+              />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <a href={"/contacts/1"}>Your Name</a>
+              </li>
+              <li>
+                <a href={"/contacts/2"}>Your Friend</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div id="content">{children}</div>
+
         <ScrollRestoration />
         <Scripts />
       </body>
@@ -40,10 +60,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default function App() {
-  return <Outlet />;
+const App: React.FC = () => {
+  return (
+    <Outlet />
+  );
 }
 
-export function HydrateFallback() {
-  return <p>Loading...</p>;
-}
+export default App;
